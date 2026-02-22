08215d5c24def48472d187b3be87fdfd4f5294e5
Alexey <alexey@a-birzgal.local>
2026-02-22 17:49:12 +0100

    fixing 403 error

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
       allureCypress(on, config, {
         //       resultsDir: "allure-results",
       });
-
+      
       return config;
     },
-
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     env: {
       apiKey: "my-secret-key",
     },
